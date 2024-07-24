c762425371b566a04ad627e4c16be2b8581dfe4c
ric-evans <emejqz@gmail.com>
2024-07-24 16:21:32 -0400

    docker load

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,12 @@ echo "entrypoint: activating docker daemon..."
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 1
 
+# load transferred docker images -- this is the simplest way to transfer host's images
+for file in /saved-images/*; do
+    echo "entrypoint: loading transferred docker image from $file"
+    docker load -i $file
+    docker images
+done
+
 echo "entrypoint: executing command: $@"
 exec "$@"
