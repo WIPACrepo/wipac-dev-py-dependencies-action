d50b1509ed6ba02cd40285403d82efa0dace808b
ric-evans <emejqz@gmail.com>
2025-08-01 15:41:31 -0500

    Better Logging, Error Handling

--- utils/get_old_pydl_file.py
+++ utils/get_old_pydl_file.py
@@ -1,66 +1,59 @@
 #!/usr/bin/env python3
 """Get the old dependency log file."""
 import argparse
+import logging
 import os
 import subprocess
 import sys
+from pathlib import Path
 
 import requests
+
+LOGGER = logging.getLogger()  # use root
+
+
+def _subproc(args: str, stdout=subprocess.DEVNULL) -> subprocess.CompletedProcess:
+    LOGGER.info(f"subprocess: {args=}")
+    return subprocess.run(
+        args.split(),
+        check=True,
+        stdout=stdout,
+        stderr=subprocess.DEVNULL,
+    )
+
+
+def _subproc_stdout(args: str) -> str:
+    res = _subproc(args, stdout=subprocess.PIPE)
+    return res.stdout.decode("utf-8").strip()
 
 
 def get_file_from_git(branch: str, filename: str, n_commits_old: int = 0) -> str | None:
     """Find the file in the branch, optionally at an old commit."""
 
     # Resolve the desired commit hash
-    if n_commits_old > 0:
-        print(f"-> fetching origin/{branch}", file=sys.stderr)
-        subprocess.run(
-            ["git", "fetch", "origin", branch, "--deepen", str(n_commits_old + 1)],
-            check=True,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-        commit_ref = subprocess.run(
-            [
-                "git",
-                "rev-list",
-                "--max-count=1",
-                f"origin/{branch}",
-                f"--skip={n_commits_old}",
-            ],
-            stdout=subprocess.PIPE,
-            text=True,
-            check=True,
-        ).stdout.strip()
-    else:
+    if n_commits_old == 0:
         commit_ref = f"origin/{branch}"
+    else:
+        LOGGER.info(f"-> fetching origin/{branch} ({n_commits_old=})")
+        # fetch, then find commit ref
+        _subproc(f"git fetch origin {branch} --deepen {n_commits_old + 1}")
+        commit_ref = _subproc_stdout(
+            f"git rev-list --max-count=1 origin/{branch} --skip={n_commits_old}"
+        )
 
-    # List all files in that commit
-    args = ["git", "ls-tree", "-r", "--name-only", commit_ref]
-    print(f"looking at {args}", file=sys.stderr)
-    result = subprocess.run(
-        args,
-        stdout=subprocess.PIPE,
-        text=True,
-        check=True,
-    )
+    # List all files in that commit ref
+    ls_tree = _subproc_stdout(f"git ls-tree -r --name-only {commit_ref}")
 
     # Search for a matching file
-    for line in result.stdout.splitlines():
+    for line in ls_tree.splitlines():
         if any(
             line.endswith(m)
             # match by basename, however old versions named the file w/o the 'py-' prefix
             for m in [f"/{filename}", f"/{filename.removeprefix('py-')}"]
         ):
-            out = subprocess.run(
-                ["git", "show", f"{commit_ref}:{line}"],
-                stdout=subprocess.PIPE,
-                text=True,
-                check=True,
-            )
-            return out.stdout
+            return _subproc_stdout(f"git show {commit_ref}:{line}")
 
-    print("-> did not find file", file=sys.stderr)
+    LOGGER.info("-> did not find file")
     return None
 
 
@@ -75,10 +68,7 @@ def get_file_from_release(repo: str, filename: str, token: str) -> str | None:
         return None
 
     for asset in r.json().get("assets", []):
-        print(
-            f"{asset=}",
-            file=sys.stderr,
-        )
+        LOGGER.info(f"{asset=}")
         if asset.get("name") == filename:
             dl_url = asset.get("browser_download_url")
             dl_r = requests.get(dl_url, headers=headers)
@@ -87,7 +77,7 @@ def get_file_from_release(repo: str, filename: str, token: str) -> str | None:
     return None
 
 
-def main() -> str:
+def main() -> None:
     parser = argparse.ArgumentParser(
         description="Get an old dependency log file from git or release."
     )
@@ -96,12 +86,20 @@ def main() -> str:
         help="The file name to retrieve.",
     )
     parser.add_argument(
-        "branch",
+        "--branch",
+        required=True,
         help="The default branch to check.",
     )
     parser.add_argument(
-        "repo",
+        "--repo",
+        required=True,
         help="GitHub repository (e.g. owner/repo).",
+    )
+    parser.add_argument(
+        "--dest",
+        type=Path,
+        required=True,
+        help="The file path to write the found file to.",
     )
     args = parser.parse_args()
 
@@ -111,29 +109,30 @@ def main() -> str:
         sys.exit(1)
 
     # look for file
-    contents = get_file_from_release(args.repo, args.filename, token)
-    if contents:
+    from_release = get_file_from_release(args.repo, args.filename, token)
+    if from_release:
         print(
             f"::notice::found file '{args.filename}' in release asset",
             file=sys.stderr,
         )
-        return contents
+        args.dest.write_text(from_release)
+        return
 
     # back-up plan: look for files in git -- start with latest commit (n=0)
     for n in range(25):  # that's probably deep enough
-        contents = get_file_from_git(args.branch, args.filename, n_commits_old=n)
-        if contents:
+        from_git = get_file_from_git(args.branch, args.filename, n_commits_old=n)
+        if from_git:
             print(
                 f"::notice::found file '{args.filename}' in '{args.branch}'",
                 file=sys.stderr,
             )
-            return contents
+            args.dest.write_text(from_git)
+            return
 
     # not to be found
-    print(f"::notice::could not find file '{args.filename}'", file=sys.stderr)
+    print(f"::notice::could not find file '{args.filename}'")
     sys.exit(2)
 
 
 if __name__ == "__main__":
-    contents = main()
-    print(contents)
+    main()
